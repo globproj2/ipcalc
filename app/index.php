a458209d1398f8258126f478732b2f63e9c9322d
globproj2 <globproj2@proton.me>
2024-01-16 13:36:23 +0000

    Commit 1.1.34

--- app/index.php
+++ app/index.php
@@ -9,7 +9,7 @@
 	<body>
 		<p><b>CALCULADORA IPv4</b></p>
 		<a href="ip.html">Aplicació de càlcul de subxarxes</a>
-		<p>Autor: globproj2 - asix2 - dacomo - gp1f4a11<p>
+		<p>Autor: globproj2 - asix2 - dacomo - gp1f4a11 - 2024<p>
                 <?php
                     echo date("l jS \of F Y h:i:s A") . "<br>";
                 ?>
